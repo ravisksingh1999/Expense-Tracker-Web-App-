3515a0d8d1be59d9b33e85c98f284e50571ca3b6
Ravi <ravishankarks199@gmail.com>
2025-07-10 22:38:50 +0530

    added backend web service in .env and apis updated accordingly

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = process.env.expense-tracker-web-app-backend_API_URL;
 
 export const register = (userData) =>
   axios.post(`${API_URL}/auth/register`, userData);
